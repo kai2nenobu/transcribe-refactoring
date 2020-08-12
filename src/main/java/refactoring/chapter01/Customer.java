7961dcce3ec79e3abd9ac2693b75f1771be70d1d
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-12 17:36:10 +0900

    p026 [Refactor] 一時変数totalAmountをprivateメソッドに置き換える
    privateメソッドの中でループしているのがパフォーマンスに影響しないか
    気になってしまうが、やはりstatementメソッド内の可読性はかなり
    上がったようにも思う。
    
    このあたりのループ系はコレクションクラスが必要そうな予感。

--- src/main/java/refactoring/chapter01/Customer.java
+++ src/main/java/refactoring/chapter01/Customer.java
@@ -20,7 +20,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
@@ -30,11 +29,20 @@ public class Customer {
 
             // この貸し出しに関する数値の表示
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         // フッタ部分の追加
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 }
