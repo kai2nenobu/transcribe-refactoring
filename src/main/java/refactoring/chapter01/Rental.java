532271277630e95eb3259401fe1f71181609302a
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-13 14:32:05 +0900

    p036 [Refactor] レンタルポイントの計算メソッドもMovieクラスに移動する

--- src/main/java/refactoring/chapter01/Rental.java
+++ src/main/java/refactoring/chapter01/Rental.java
@@ -22,10 +22,6 @@ public class Rental {
     }
 
     int getFrequentRenterPoints() {
-        // 新作を二日以上借りた場合はボーナスポイント
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() >1)
-            return 2;
-        else
-            return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
