515a66e3d22c2581c443e03c485374cf85777763
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-15 23:49:55 +0900

    p122 [Refactor] 一時変数を問い合わせメソッドに置き換える
    一時変数basePriceをbasePriceメソッドに置き換えるだけなら、メリット
    あるかな～、って感じ。しかしdiscountFactorをメソッドに置き換えると
    getPriceメソッドに条件分岐がなくなり可読性が爆上がりする。
    discountForメソッドを見れば条件によりディスカウント率が異なることが
    ぱっと分かってコードが格段に把握しやすい。
    これが抽象度が揃った記述の威力ということなのか。
    
    先にbasePriceメソッドに置き換えているおかげで、discountFactorメソッドの
    抽出が簡単になっている。こうやって2つの一時変数の置き換えを体験しないと
    わからないことだった。

--- src/main/java/refactoring/chapter06/ReplaceTempWithQuery.java
+++ src/main/java/refactoring/chapter06/ReplaceTempWithQuery.java
@@ -6,11 +6,16 @@ package refactoring.chapter06;
  */
 public class ReplaceTempWithQuery {
     double getPrice() {
-        int basePrice = _quantity * _itemPrice;
-        double discountFactor;
-        if (basePrice > 1000) discountFactor = 0.95;
-        else discountFactor = 0.98;
-        return basePrice * discountFactor;
+        return basePrice() * discountFactor();
+    }
+
+    private int basePrice() {
+        return _quantity * _itemPrice;
+    }
+
+    private double discountFactor() {
+        if (basePrice() > 1000) return 0.95;
+        else return 0.98;
     }
 
     /* 以下は書籍では記述されていない無関係な部分 */
