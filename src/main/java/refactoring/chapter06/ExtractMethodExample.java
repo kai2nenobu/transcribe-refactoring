961045cbc97d3dece24fff1b21603441c9b42154
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-15 16:56:48 +0900

    p112 [Impl] 「メソッドの抽出」の例を追加する

---
+++ src/main/java/refactoring/chapter06/ExtractMethodExample.java
@@ -0,0 +1,40 @@
+package refactoring.chapter06;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+
+/**
+ * 「メソッドの抽出」の例
+ */
+public class ExtractMethodExample {
+    void printOwing() {
+        Enumeration e = _orders.elements();
+        double outstanding = 0.0;
+
+        // バナーの印刷
+        System.out.println("*************************");
+        System.out.println("***** Customer Owes *****");
+        System.out.println("*************************");
+
+        // 未払金の計算
+        while (e.hasMoreElements()) {
+            Order each = (Order) e.nextElement();
+            outstanding += each.getAmount();
+        }
+
+        // 明細の印刷
+        System.out.println("name:" + _name);
+        System.out.println("amount:" + outstanding);
+    }
+
+    /* 以下は書籍では記述されていない無関係な部分 */
+    private Vector _orders = new Vector();
+    private String _name = "Dummy";
+
+    private class Order {
+        public double getAmount() {
+            return 0.0;
+        }
+    }
+}
