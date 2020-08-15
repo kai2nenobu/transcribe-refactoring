e615606022327ec2340212b0774e2cae710d66a8
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-16 00:27:41 +0900

    p125 [Refactor] 価格計算の要素をそれぞれ一時変数にする
    priceメソッドのコメントがいらなくなるほどわかりやすくなった。
    このリファクタリングは普段のコーディングでもわりとやりやすい
    リファクタリングであると思う。
    
    この例はとてもシンプルだったので一時変数の導入がやりやすかったが、
    長大なメソッドで複雑な計算の場合、どう抽出していくかはなかなか
    判断しづらい気もする。
    前提として「価格は、基本価格 - 数量割引 + 送料」という仕様があるので、
    これらを抽出の手がかりとしていくのがいいのかな。

--- src/main/java/refactoring/chapter06/IntroduceExplainingVariableExample.java
+++ src/main/java/refactoring/chapter06/IntroduceExplainingVariableExample.java
@@ -6,10 +6,10 @@ package refactoring.chapter06;
  */
 public class IntroduceExplainingVariableExample {
     double price() {
-        // 価格(price)は、基本価格(base price) - 数量割引(quantity discount) + 送料(shipping)
-        return _quantity * _itemPrice -
-                Math.max(0, _quantity - 500) * _itemPrice * 0.05 +
-                Math.min(_quantity * _itemPrice * 0.1, 100.0);
+        double basePrice = _quantity * _itemPrice;
+        double quantityDiscount = Math.max(0, _quantity - 500) * _itemPrice * 0.05;
+        double shipping = Math.min(_quantity * _itemPrice * 0.1, 100.0);
+        return basePrice - quantityDiscount + shipping;
     }
 
     /* 以下は書籍では記述されていない無関係な部分 */
