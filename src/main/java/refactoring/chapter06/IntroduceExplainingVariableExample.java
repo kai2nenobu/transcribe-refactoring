893b3c4e2a069119eb696dbed7a0ae60d120e3d3
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-16 00:47:00 +0900

    p126 [Refactor] 「メソッドの抽出」で計算をわかりやすくする
    たしかに「説明変数の導入」に比べて、クラス内でメソッドが使い回せる分、
    便利な気がする。まあメソッドの抽出のしやすさに応じてやりやすい方を使っていけば
    いいということか。

--- src/main/java/refactoring/chapter06/IntroduceExplainingVariableExample.java
+++ src/main/java/refactoring/chapter06/IntroduceExplainingVariableExample.java
@@ -6,10 +6,19 @@ package refactoring.chapter06;
  */
 public class IntroduceExplainingVariableExample {
     double price() {
-        // 価格(price)は、基本価格(base price) - 数量割引(quantity discount) + 送料(shipping)
-        return _quantity * _itemPrice -
-                Math.max(0, _quantity - 500) * _itemPrice * 0.05 +
-                Math.min(_quantity * _itemPrice * 0.1, 100.0);
+        return basePrice() - quantityDiscount() + shipping();
+    }
+
+    private double quantityDiscount() {
+        return Math.max(0, _quantity - 500) * _itemPrice * 0.05;
+    }
+
+    private double shipping() {
+        return Math.min(basePrice() * 0.1, 100.0);
+    }
+
+    private double basePrice() {
+        return _quantity * _itemPrice;
     }
 
     /* 以下は書籍では記述されていない無関係な部分 */
