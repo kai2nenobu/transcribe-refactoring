55eaf3dd695f61211c32a295d34385672ed1fdea
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-23 17:52:35 +0900

    p137 [Refactor] メソッドオブジェクトにメソッドを移動する
    元のメソッドはメソッドオブジェクトに処理を委譲する。なるほど、
    こうすると新しいメソッドでは一切一時変数がなくなり、
    すべてフィールドに対する操作になるわけだ。

--- src/main/java/refactoring/chapter06/ReplaceMethodWithMethodObjectExample.java
+++ src/main/java/refactoring/chapter06/ReplaceMethodWithMethodObjectExample.java
@@ -3,13 +3,7 @@ package refactoring.chapter06;
 public class ReplaceMethodWithMethodObjectExample {
     class Account {
         int gamme(int inputVal, int quantity, int yearToDate) {
-            int importantValue1 = (inputVal * quantity) + delta();
-            int importantValue2 = (inputVal * yearToDate) + 100;
-            if ((yearToDate - importantValue1) > 100)
-                importantValue2 -= 20;
-            int importantValue3 = importantValue2 * 7;
-            // 略
-            return importantValue3 - 2 * importantValue1;
+            return new Gamma(this, inputVal, quantity, yearToDate).compute();
         }
 
         /* 以下は書籍では記述されていない無関係な部分 */
@@ -32,6 +26,16 @@ public class ReplaceMethodWithMethodObjectExample {
             inputVal = inputValArg;
             quantity = quantityArg;
             yearToDate = yearToDateArg;
+        }
+
+        int compute() {
+            importantValue1 = (inputVal * quantity) + _account.delta();
+            importantValue2 = (inputVal * yearToDate) + 100;
+            if ((yearToDate - importantValue1) > 100)
+                importantValue2 -= 20;
+            importantValue3 = importantValue2 * 7;
+            // 略
+            return importantValue3 - 2 * importantValue1;
         }
     }
 }
