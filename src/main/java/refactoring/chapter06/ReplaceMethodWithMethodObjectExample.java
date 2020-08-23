1e49b0c33d0ecc5c044df4fc8b3454d5ba0b0e3b
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-23 17:46:32 +0900

    p137 [Refactor] メソッドオブジェクト用のクラスを定義する
    メソッドオブジェクト用のクラスはフィールドに
    
    - 元のクラスのオブジェクト
    - メソッドの一時変数すべて
    - メソッドのパラメータすべて
    
    を持つようにすればいい。これは手順通りやればいいので割とシンプル。

--- src/main/java/refactoring/chapter06/ReplaceMethodWithMethodObjectExample.java
+++ src/main/java/refactoring/chapter06/ReplaceMethodWithMethodObjectExample.java
@@ -17,4 +17,21 @@ public class ReplaceMethodWithMethodObjectExample {
             return 0;
         }
     }
+
+    class Gamma {
+        private final Account _account;
+        private int inputVal;
+        private int quantity;
+        private int yearToDate;
+        private int importantValue1;
+        private int importantValue2;
+        private int importantValue3;
+
+        Gamma(Account source, int inputValArg, int quantityArg, int yearToDateArg) {
+            _account = source;
+            inputVal = inputValArg;
+            quantity = quantityArg;
+            yearToDate = yearToDateArg;
+        }
+    }
 }
