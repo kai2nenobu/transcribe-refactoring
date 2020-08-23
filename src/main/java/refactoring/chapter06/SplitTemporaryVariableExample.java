1b460e34d0ea2f935b517eb54cc407bc480cbdc1
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-23 16:19:44 +0900

    p130 [Refactor] 最初の代入の変数名を変更したらfinalにする
    こういう一時変数の使い回しは実務コードでよく見る。
    こうやって一時変数の責務をわかりやすいものにするのは、
    比較的やりやすいリファクタリングをだと思われる。

--- src/main/java/refactoring/chapter06/SplitTemporaryVariableExample.java
+++ src/main/java/refactoring/chapter06/SplitTemporaryVariableExample.java
@@ -6,13 +6,13 @@ package refactoring.chapter06;
 public class SplitTemporaryVariableExample {
     double getDistanceTravelled(int time) {
         double result;
-        double acc = _primaryForce / _mass;
+        final double primaryAcc = _primaryForce / _mass;
         int primaryTime = Math.min(time, _delay);
-        result = 0.5 * acc * primaryTime * primaryTime;
+        result = 0.5 * primaryAcc * primaryTime * primaryTime;
         int secondaryTime = time - _delay;
         if (secondaryTime > 0) {
-            double primaryVel = acc * _delay;
-            acc = (_primaryForce + _secondaryForce) / _mass;
+            double primaryVel = primaryAcc * _delay;
+            double acc = (_primaryForce + _secondaryForce) / _mass;
             result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
         }
         return result;
